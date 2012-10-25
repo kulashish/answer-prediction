31837c510e0fd06faeec80cce6595d36bbaf3d97
Ashish Kulkarni <kulashish@gmail.com>
2012-10-25 11:21:46 +0530

    serialize training instances to ARFF

--- src/in/ac/iitb/cse/ml/data/TrainingSet.java
+++ src/in/ac/iitb/cse/ml/data/TrainingSet.java
@@ -9,9 +9,18 @@ import java.util.ArrayList;
 
 import weka.core.Attribute;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class TrainingSet {
 	private Instances trainingInstances;
+
+	public Instances getTrainingInstances() {
+		return trainingInstances;
+	}
+
+	public void setTrainingInstances(Instances trainingInstances) {
+		this.trainingInstances = trainingInstances;
+	}
 
 	/*
 	 * Creates TrainingSet instance from the CSV files present in a folder whose
@@ -46,18 +55,32 @@ public class TrainingSet {
 				trainingInstances.add(studentInstance.createWekaInstance());
 		}
 		data.close();
-		System.out.println("Populated " + trainingInstances.size() + " instances");
+		System.out.println("Populated " + trainingInstances.size()
+				+ " instances");
 	}
 
+	/*
+	 * Expects two arguments - folder containing the CSV files and path to
+	 * output ARFF file
+	 */
 	public static void main(String args[]) {
 		String path = args[0];
+		String arffFile = args[1];
 		TrainingSet train = null;
 		try {
 			System.out.println("Creating training set...");
 			train = new TrainingSet(path);
+			train.serializeARFF(arffFile);
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
 		System.out.println("Training set created!");
+	}
+
+	private void serializeARFF(String arffFile) throws IOException {
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(trainingInstances);
+		saver.setFile(new File(arffFile));
+		saver.writeBatch();
 	}
 }
